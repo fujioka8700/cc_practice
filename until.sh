7e8e21c6b177d80dc032fe114ad1cf2dced61585
lenovo ideapad <ideapad.lenovo.s540@gmail.com>
2022-01-10 07:08:47 +0900

    [add] untilループ

---
+++ until.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+a=0
+
+until [ ! $a -lt 5 ]
+do
+    echo $a
+    a=`expr $a + 1`
+done
--- whileEx.sh
+++ whileEx.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-# 続きはwhileの復習から
+# whileループ
+a=0
+
+# -ltは、より小さい
+while [ $a -lt 5 ];
+do
+    a=`expr $a + 1`
+    echo $a
+done
